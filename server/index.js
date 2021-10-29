7186b52c7fce3aa5fce1f0af72c1ab73e85cae95
eswar2001 <eswar.abisheak94@gmail.com>
2021-10-29 15:54:17 +0530

    generate hash

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
-
+const bodyParser = require('body-parser');
+const md5 = require('md5');
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -9,3 +10,24 @@ app.use(cors());
 app.listen(PORT, () => {
     console.log('Server started');
 });
+
+app.get('/', (req, res) => {
+    res.send('GET');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.status(200).send('Image Recieved');
+});
+
+app.post('/create', (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    res.status(200).send(`http://localhost:8000/${md5(name).slice(0, 6)}`);
+});
+
+app.get('/find/:string/', (req, res) => {
+    res.send(`Found something at ${req.path}`);
+})
+// http://localhost:8000/
+// npm i --save express cors nodemon 
